f9902f91313f708284340653bb0c39bfb8db2ac1
gaurmand <gaurav_amwan@hotmail.com>
2020-06-26 19:20:54 -0700

    Modify GeoData to only use "real" countries
    Previosuly we used every single countrue in the ne_50m_admin_countries geoGJON file. Filtered the master
    array of countires to only contains countries of the type 'Country' and 'Sovereign Country'.
    
    Removed GeoData.NUM_COUNTRIES const that was set to the length of array. Instead initialize a numCountries
    property inthe GeoData constructor.
    
    Also modify getCountry() to return the entire country object, instead of just the ISO_A3 code.

--- src/geodata.js
+++ src/geodata.js
@@ -1,23 +1,25 @@
 class GeoData {
   constructor() {
-    this.countries = GeoData.COUNTRIES_50M.features.map(feature => feature.properties);
+    this.countries = GeoData.COUNTRIES_50M.features.map(feature => feature.properties).filter(country => country.TYPE === 'Country' || country.TYPE === 'Sovereign country');
+    this.numCountries = this.countries.length;
     console.log(this.countries)
+
   }
 
   findCountry(ISO_A3) {
     return this.countries.find(country => country.ISO_A3 === ISO_A3);
   }
 
-  getRandomCountries(numCountries) {
-    return GeoData.getUniqueRandomArr(numCountries, GeoData.NUM_COUNTRIES).map(i => this.getCountry(i));
+  getRandomCountries(num) {
+    return GeoData.getUniqueRandomArr(num, this.numCountries).map(i => this.getCountry(i));
   }
 
   getRandomCountry() {
-    return this.getCountry(GeoData.getRandomInt(GeoData.NUM_COUNTRIES));
+    return this.getCountry(GeoData.getRandomInt(this.numCountries));
   }
 
   getCountry(i) {
-    return this.countries[i].ISO_A3;
+    return this.countries[i];
   }
 
   static getUniqueRandomArr(numElems, max) {
@@ -53,8 +55,6 @@ GeoData.RIVERS_50M = require('../data/GeoJSON/ne_50m_rivers_lake_centerlines.jso
 GeoData.RIVERS_110M = require('../data/GeoJSON/ne_110m_rivers_lake_centerlines_35.json');
 GeoData.LAKES_50M = require('../data/GeoJSON/ne_50m_lakes.json');
 GeoData.LAKES_110M = require('../data/GeoJSON/ne_110m_lakes_47.json');
-
-GeoData.NUM_COUNTRIES = GeoData.COUNTRIES_50M.features.length;
 
 module.exports = {
   GeoData
